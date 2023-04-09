a2b7cbcf2e6b68a00c4be6dbaf48ecd3344085c4
nafiyathehreem26 <nafiyathehreem2003@gmail.com>
2023-04-09 10:43:42 +0530

    first commit

--- php/profile.php
+++ php/profile.php
@@ -1,4 +1,18 @@
 <?php
-
-header('Location: http://localhost/guvi/index.html');
+  // connect to mongodb
+  header('Location: http://localhost/guvi/index.html');
+  $m = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+  echo "Connection to database successfully";
+  // select a database
+  $db = $m->profiledb;
+  echo "Database profiledb selected";
+  $collection = $db->profile;
+  echo "Collection selected successfully";
+  $document = array(
+  "name" =>$_POST["name"],
+  "designation" => $_POST["designation"],
+  "age" => $_POST["age"]
+ );
+$collection->insert($document);
+echo "Document inserted successfully";
 ?>
